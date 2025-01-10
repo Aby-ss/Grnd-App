8a1eb0649cd9273c1e23ce5dedc9aac3ceb3382f
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-01-10 22:58:17 +0400

    Added App code + components code to new folder ✅

---
+++ Grnd-App/app/_layout.jsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import TabBar from '../components/TabBar'
+
+const _layout = () => {
+  return (
+    <Tabs
+        screenOptions={{headerShown: false}}
+        tabBar={props=> <TabBar {...props} />}
+    >
+        <Tabs.Screen
+            name="Home"
+            options={{
+                title: "Home"
+            }}
+        />
+    </Tabs>
+  )
+}
+
+export default _layout
